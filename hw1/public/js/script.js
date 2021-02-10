abcc501909a814ecaccee5a69de1ba9f248ccd77
DarrrkS <mikhail.anikin15@gmail.com>
2021-02-10 12:16:44 +0300

    hw1

---
+++ hw1/public/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#test-button1").click(function () {
+        confirm('А вы умеете нажимать на кнопочки?');
+    })
+    $("#test-button2").click(function () {
+        alert('Поздравляю! Вы умеете нажимать на кнопочки!')
+    })
+})
---
+++ hw1/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Страничка 1' });
+});
+
+router.get('/test', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/test.html'));
+});
+
+module.exports = router;
