333fc98669f69be9c86f1672e6c53a3154309f3a
DarrrkS <mikhail.anikin15@gmail.com>
2021-02-14 23:58:39 +0300

    hw1 update

--- hw1/public/js/script.js
+++ hw1/public/js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
     $("#test-button1").click(function () {
-        confirm('А вы умеете нажимать на кнопочки?');
+        let check = $(this).data('check') | 0;
+        if (check) {
+            if (confirm('Уже умеешь, мы поняли')) {
+                $(this).html('У нас тут буйный');
+                $(this).data('check', check);
+            }
+        } else {
+            if (confirm('А вы умеете нажимать на кнопочки?')) {
+                $(this).html('Пакуем его, ребят');
+                $(this).data('check', check);
+            }
+        }
+        check++;
+        $(this).data('check', check);
     })
     $("#test-button2").click(function () {
         alert('Поздравляю! Вы умеете нажимать на кнопочки!')
